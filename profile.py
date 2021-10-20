8e5d0fa93a9f2983987cf1ed2d7e4b8375351300
Virginia Lopez Nadal <vlopeznadal@icloud.com>
2021-10-20 18:53:54 -0500

    Completed profile.py

--- profile.py
+++ profile.py
@@ -17,10 +17,12 @@ def profile():
     """Return results from profile form."""
 
     fullname = request.json['name']
+    occupation = request.json['occupation']
+    age = request.json['age']
     # TODO: get the values from the rest of the form
     # Add them to jsonify
     
-    return jsonify({'fullname': fullname, })
+    return jsonify({'fullname': fullname, 'occupation': occupation, 'age': age})
 
 
 
